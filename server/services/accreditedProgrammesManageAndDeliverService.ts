6d5a2b1dda0bd92ba64b9e6c478deefe6334d4e2
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2025-09-11 12:22:38 +0100

    update types file and create post delivery location preferences service method

--- server/services/accreditedProgrammesManageAndDeliverService.ts
+++ server/services/accreditedProgrammesManageAndDeliverService.ts
@@ -21,6 +21,7 @@ import {
   SentenceInformation,
   ThinkingAndBehaviour,
   UpdateAvailability,
+  CreateDeliveryLocationPreferences,
 } from '@manage-and-deliver-api'
 import { CaselistFilterParams } from '../caselist/CaseListFilterParams'
 import config, { ApiConfig } from '../config'
@@ -245,6 +246,19 @@ export default class AccreditedProgrammesManageAndDeliverService {
       path: `/risks-and-needs/${crn}/offence-analysis`,
       headers: { Accept: 'application/json' },
     })) as OffenceAnalysis
+  }
+
+  async createDeliveryLocationPreferences(
+    username: Express.User['username'],
+    referralId: string,
+    createDeliveryLocationPreferences: CreateDeliveryLocationPreferences,
+  ): Promise<CreateDeliveryLocationPreferences> {
+    const restClient = await this.createRestClientFromUsername(username)
+    return (await restClient.post({
+      path: `/delivery-location-preferences/referral/${referralId}`,
+      headers: { Accept: 'application/json' },
+      data: createDeliveryLocationPreferences,
+    })) as CreateDeliveryLocationPreferences
   }
 
   async getDeliveryLocationPreferences(
